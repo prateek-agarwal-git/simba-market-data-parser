5697622a889b331be241a8651ae03537b14b7290
Prateek Agarwal <pratekag@gmail.com>
2024-01-20 02:17:24 +0530

    Create schema bit masks and schema enums file

---
+++ include/schema_bitmasks.h
@@ -0,0 +1,48 @@
+#include <cstdint>
+namespace simba_schema::bitmasks {
+
+enum MDFlagsSet : std::uint64_t {
+  DAY = (1 << 0),
+  IOC = (1 << 1),
+  NonQuote = (1 << 2),
+  EndOfTransaction = 1 << 12,
+  DueToCrossCancel = 1 << 13,
+  SecondLeg = 1 << 14,
+  FOK = 1 << 19,
+  Replace = 1 << 20,
+  Cancel = 1 << 21,
+  MassCancel = 1 << 22,
+  Negotiated = 1 << 26,
+  MultiLeg = 1 << 27,
+  CrossTrade = 1 << 29,
+  COD = 1 << 32,
+  ActiveSide = 1 << 41,
+  PassiveSide = 1 << 42,
+  Syntheic = 1 << 45,
+  RFS = 1 << 46,
+  SyntheticPassive = 1 << 57,
+  BOC = 1 << 60,
+  DuringDiscreteAuction = 1 << 62
+};
+
+enum MDFlags2Set : std::uint64_t { Zero = 1 << 0 };
+
+enum FlagsSet : std::uint64_t {
+  EveningOrMorningSession = 1 << 0,
+  AnnonymousTrading = 1 << 4,
+  PrivateTrading = 1 << 5,
+  DaySession = 1 << 6,
+  // name collision for multileg may be use enum class
+  Multileg = 1 << 8,
+  Collateral = 1 << 18,
+  IntradayExercise = 1 << 19
+};
+enum MsgFlagsSet : std::uint16_t {
+  LastFragment = 1 << 0,
+  StartOfSnapShot = 1 << 1,
+  EndOfSnapShot = 1 << 2,
+  IncrementalPacket = 1 << 3,
+  PossDupFlag = 1 << 4
+};
+
+} // namespace simba_schema::bitmasks
---
+++ include/schema_enums.h
@@ -0,0 +1,54 @@
+#include <cstdint>
+
+namespace simba_schema::enums {
+
+enum MDUpdateAction : std::uint8_t { New = 0, Change = 1, Delete = 2 };
+enum MDEntryType : char {
+  // verify this
+  Bid = '0',
+  Offer = '1',
+  EmptyBook = 'J'
+
+};
+
+enum SecurityAltIdSource : char { ISIN = '4', ExchangeSymbol = '8' };
+
+enum SecurityTradingStatus : std::uint8_t {
+  // schema type is uint8_t NULL
+  TradingHalt = 2,
+  ReadyToTrade = 17,
+  NotAvailableForTrading = 18,
+  NotTradedonThisMarket = 19,
+  UnknownOrInvalid = 20,
+  PreOpen = 21,
+  DiscreteAuctionOpen = 119,
+  DiscreteAuctionClose = 121,
+  InstrumentHalt = 122
+};
+
+enum TradingSessionId : std::uint8_t {
+  // schema type is uint8_t NULL
+  Day = 1,
+  Morning = 3,
+  Evening = 5
+};
+
+enum MarketSegmentId : char { Derivatives = 'D' };
+
+enum TradeSesStatus : std::uint8_t {
+  Halted = 1,
+  Open = 2,
+  Closed = 3,
+  PreOpen = 4
+};
+
+enum TradeSesEvent :std::uint8_t{
+  // schema type is uint8_t NULL
+  TradingResumes = 0,
+  ChangeOfTradingSession = 1,
+  ChangeOfTradingStatus = 3
+};
+
+enum NegativePrices : std::uint8_t { NotEligibile = 0, Eligible = 1 };
+
+} // namespace simba_schema
